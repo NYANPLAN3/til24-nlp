74866c7706c76f5c0edebd3ae9644f6c8023e790
John-Henry Lim <42513874+Interpause@users.noreply.github.com>
2024-06-02 23:02:20 +0000

    remove spacy, put full color list, simplify regex

--- til24_nlp/colorcorrection.py
+++ til24_nlp/colorcorrection.py
@@ -1,19 +1,21 @@
+import pkgutil
 import re
+
+data = pkgutil.get_data(__package__, 'colors.txt')
+COLORS = [w.strip() for w in data.decode('utf-8').strip().split('\n')]
+
 
 def replace_misheard_colors(text):
     # Define the colors to look for
-    colors = ['black', 'white', 'red', 'blue', 'green', 'yellow', 'pink', 'purple', 'orange', 'brown', 'grey']
-    
+
+    # NOTE: changed to handle specifically white
     # Create a regex pattern to find adjacent colors separated by a space
-    color_pattern = r'\b({})\s+({})\b'.format('|'.join(colors), '|'.join(colors))
-    
+    color_pattern = r'\b({})\s+white\b'.format('|'.join(COLORS))
+
     def replace_white(match):
-        color1, color2 = match.groups()
-        if (color1 == 'black' and color2 == 'white'):
-            return match.group(0).replace('white', 'light')
-        return match.group(0)
-    
+        return match.group(0).replace('white', 'light')
+
     # Use re.sub with the custom replace function
     result = re.sub(color_pattern, replace_white, text)
-    
+
     return result
